8a1c6cb5d7a3588ba344eb289d1104713ccdd9e4
Giuseppe Samela <giuseppe.samela@gmail.com>
2017-03-17 17:40:22 +0100

    New implementation of ExoPlayer inside the main app The initialization of the player is now done by other classes; MyExoPlayer is now used to create the player with our parameters.

--- app/src/main/java/fr/unice/i3s/uca4svr/toucan_vr/Minimal360VideoActivity.java
+++ app/src/main/java/fr/unice/i3s/uca4svr/toucan_vr/Minimal360VideoActivity.java
@@ -21,34 +21,13 @@
 package fr.unice.i3s.uca4svr.toucan_vr;
 
 import android.content.res.AssetFileDescriptor;
-import android.media.MediaCodec;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Surface;
 
-import com.google.android.exoplayer.DefaultLoadControl;
 import com.google.android.exoplayer.ExoPlaybackException;
 import com.google.android.exoplayer.ExoPlayer;
-import com.google.android.exoplayer.LoadControl;
-import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
-import com.google.android.exoplayer.MediaCodecSelector;
-import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
-import com.google.android.exoplayer.chunk.ChunkSampleSource;
-import com.google.android.exoplayer.chunk.ChunkSource;
-import com.google.android.exoplayer.chunk.FormatEvaluator;
-import com.google.android.exoplayer.dash.DashChunkSource;
-import com.google.android.exoplayer.dash.DefaultDashTrackSelector;
-import com.google.android.exoplayer.dash.mpd.MediaPresentationDescription;
-import com.google.android.exoplayer.dash.mpd.MediaPresentationDescriptionParser;
-import com.google.android.exoplayer.extractor.ExtractorSampleSource;
-import com.google.android.exoplayer.upstream.Allocator;
-import com.google.android.exoplayer.upstream.AssetDataSource;
-import com.google.android.exoplayer.upstream.DataSource;
-import com.google.android.exoplayer.upstream.DefaultAllocator;
-import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer.upstream.DefaultUriDataSource;
-import com.google.android.exoplayer.util.ManifestFetcher;
 import com.google.android.exoplayer.util.Util;
 
 import org.gearvrf.GVRActivity;
@@ -57,7 +36,6 @@ import org.gearvrf.scene_objects.GVRVideoSceneObjectPlayer;
 
 import java.io.IOException;
 
-import fr.unice.i3s.uca4svr.toucan_vr.Minimal360Video;
 
 public class Minimal360VideoActivity extends GVRActivity {
 
@@ -146,50 +124,30 @@ public class Minimal360VideoActivity extends GVRActivity {
     }
 
     private GVRVideoSceneObjectPlayer<ExoPlayer> makeExoPlayer() {
-        final ExoPlayer player = ExoPlayer.Factory.newInstance(RENDERER_COUNT, MIN_BUFFER_MS, MIN_REBUFFER_MS);
+        final ExoPlayer player;
+        String userAgent = Util.getUserAgent(this, "TOUCAN-VR");
+        final String uriManifest = "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd";
 
-        final Allocator allocator = new DefaultAllocator(BUFFER_SEGMENT_SIZE);
-        final String userAgent = Util.getUserAgent(this, "TOUCAN-VR");
+        /** The ExoPlayer is now initialized inside the MyExoPlayer class, based on the DemoPlayer app provided by the framework.
+         *  Other than playing a local file, we can now stream a 360-video from a manifest file.
+         *  Constructors for DashRendererBuilder and ExtractorRendererBuilder (if needed) have been extended to accomodate new parameters.
+         *
+         *  The following parameters are needed by the constructor:
+         *  @param uriManifest          //String with the url of the DASH manifest
+         *  @param bufferSegmentSize
+         *  @param bufferSegmentCount   //the product segment_size * segment_count is the total buffer size, in bytes
+         *  @param minBufferMs          //minimum amount of data that must be buffered before the playback can start (default: 1000 ms)
+         *  @param minRebufferMs        //minimum amount of data needed to restart playback after a rebuffering event (default: 5000 ms)
+         **/
 
-        final MediaCodecVideoTrackRenderer videoRenderer;
-        final MediaCodecAudioTrackRenderer audioRenderer;
-
-        if (USE_LOCAL_FILE) {
-            final AssetDataSource dataSource = new AssetDataSource(this);
-            final ExtractorSampleSource sampleSource = new ExtractorSampleSource(Uri.parse("asset:///videos_s_3.mp4"),
-                    dataSource, allocator, VIDEO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE);
-            videoRenderer = new MediaCodecVideoTrackRenderer(this, sampleSource,
-                    MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-            audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource, MediaCodecSelector.DEFAULT);
+        if (USE_LOCAL_FILE) {      // N.B. for now the parameters are constants values
+            myPlayer = new MyExoPlayer(new ExtractorRendererBuilder(this, userAgent, Uri.parse("asset:///videos_s_3.mp4"),
+                    BUFFER_SEGMENT_SIZE, BUFFER_SEGMENT_COUNT), MIN_BUFFER_MS, MIN_REBUFFER_MS);
         } else {
-            final String uriManifest = "https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd";
-            final LoadControl loadControl = new DefaultLoadControl(allocator);
-            final DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-
-            final MediaPresentationDescriptionParser parser = new MediaPresentationDescriptionParser();
-            final DefaultUriDataSource manifestDataSource = new DefaultUriDataSource(this, userAgent);
-            final ManifestFetcher<MediaPresentationDescription> manifestFetcher = new ManifestFetcher<>(uriManifest,
-                    manifestDataSource, parser);
-
-            final DataSource videoDataSource = new DefaultUriDataSource(this, bandwidthMeter, userAgent);
-            final ChunkSource videoChunkSource = new DashChunkSource(manifestFetcher,
-                    DefaultDashTrackSelector.newVideoInstance(this, true, false), videoDataSource,
-                    new FormatEvaluator.AdaptiveEvaluator(bandwidthMeter), LIVE_EDGE_LATENCY_MS, 0, null, null, 0);
-            final ChunkSampleSource videoSampleSource = new ChunkSampleSource(videoChunkSource, loadControl,
-                    VIDEO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE);
-            videoRenderer = new MediaCodecVideoTrackRenderer(this, videoSampleSource, MediaCodecSelector.DEFAULT,
-                    MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-
-            final DataSource audioDataSource = new DefaultUriDataSource(this, bandwidthMeter, userAgent);
-            final ChunkSource audioChunkSource = new DashChunkSource(manifestFetcher,
-                    DefaultDashTrackSelector.newAudioInstance(), audioDataSource, null, LIVE_EDGE_LATENCY_MS,
-                    0, null, null, 1);
-            final ChunkSampleSource audioSampleSource = new ChunkSampleSource(audioChunkSource,
-                    loadControl, AUDIO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE);
-            audioRenderer = new MediaCodecAudioTrackRenderer(audioSampleSource, MediaCodecSelector.DEFAULT);
+            myPlayer = new MyExoPlayer(new DashRendererBuilder(this, userAgent, uriManifest, null,
+                    BUFFER_SEGMENT_SIZE, BUFFER_SEGMENT_COUNT), MIN_BUFFER_MS, MIN_REBUFFER_MS);
         }
-
-        player.prepare(videoRenderer, audioRenderer);
+        player = myPlayer.prepare();
 
         return new GVRVideoSceneObjectPlayer<ExoPlayer>() {
             @Override
@@ -228,8 +186,7 @@ public class Minimal360VideoActivity extends GVRActivity {
                     public void onPlayerError(ExoPlaybackException error) {
                     }
                 });
-
-                player.sendMessage(videoRenderer, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE, surface);
+                myPlayer.setSurface(surface);
             }
 
             @Override
@@ -255,17 +212,14 @@ public class Minimal360VideoActivity extends GVRActivity {
     }
 
     private GVRVideoSceneObjectPlayer<?> videoSceneObjectPlayer;
+    private MyExoPlayer myPlayer;
 
-    static final boolean USE_EXO_PLAYER = true;     //boolean value to select whether to play the video with ExoPlayer or MediaPlayer
+    static final boolean USE_EXO_PLAYER = true;    //boolean value to select whether to play the video with ExoPlayer or MediaPlayer
     static final boolean USE_LOCAL_FILE = false;    //boolean value to select whether to play a local video, or stream it from the internet
 
-    static final int RENDERER_COUNT = 2;    //number of TrackRenderer objects that will be passed to "prepare"
-    static final int MIN_BUFFER_MS = 1500;  //minimum amount of data that must be buffered before the playback can start
-    static final int MIN_REBUFFER_MS = 5000;  //minimum amount of data needed to restart playback after a rebuffering event
+    static final int MIN_BUFFER_MS = 1500;
+    static final int MIN_REBUFFER_MS = 5000;
 
     static final int BUFFER_SEGMENT_SIZE = 64 * 1024;
-    static final int VIDEO_BUFFER_SEGMENTS = 256;   //the product segment_size * buffer_segments is the total buffer size, in byte
-    static final int AUDIO_BUFFER_SEGMENTS = 54;
-    static final int LIVE_EDGE_LATENCY_MS = 30000;  /*for live streams, number of ms that the playback should lag behind the "live edge"
-                                                    (i.e. the end of the most recently defined media in the manifest)*/
+    static final int BUFFER_SEGMENT_COUNT = 256;
 }
