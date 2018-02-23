ac231652ce8afcf4ef7b2e2fa1fd188bccc0e565
sassatelli <sassatelli@i3s.unice.fr>
2018-02-23 16:27:44 +0100

    Update DefaultDashSRDChunkSource.java

--- app/src/main/java/com/google/android/exoplayer2/source/dash/DefaultDashSRDChunkSource.java
+++ app/src/main/java/com/google/android/exoplayer2/source/dash/DefaultDashSRDChunkSource.java
@@ -14,6 +14,9 @@ package com.google.android.exoplayer2.source.dash;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ 
+ * Modifications:
+ * Copyright 2017 Université Nice Sophia Antipolis (member of Université Côte d'Azur), CNRS
  */
 
 import android.net.Uri;
